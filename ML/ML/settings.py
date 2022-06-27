c5107b54114ba503543c8bdbfb02ae9c53685eec
frontyoon <issou06261@gmail.com>
2022-06-28 08:28:44 +0900

    change files

--- ML/ML/settings.py
+++ ML/ML/settings.py
@@ -20,7 +20,24 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vknx5gyhdacg)!(p#wj3w-scw-1hxs*qm(8mjhet2c1v1*$*6n'
+import os, json
+from django.core.exceptions import ImproperlyConfigured
+
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json') # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,11 +55,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    # App
+    # Add App
     'api',
-
-    # restframework
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- ML/api/views.py
+++ ML/api/views.py
@@ -1,21 +1,24 @@
 from django.views import View
 import json
-#from rest_framework.response import Response
-#from serializers import emotion_objectSerializer
 from .ai.djanog_fn import _output
 from django.http import JsonResponse
+import os
 
-
+# ai 함수 실행 View
 class PredictView(View):
     def get(self, request):
- 
+        # body의 json값 받아와서 url 이라는 변수에 저장
         json_object = json.loads(request.body)
         url = json_object['imageUrl']
 
-        model_pth = 'ai/best_model.pth'
-
+        # 모델 위치 상대 경로
+        file_path = os.path.abspath(__file__)
+        folder_path = os.path.dirname(file_path)
+        model_pth = os.path.join(folder_path, 'ai/best_model.pth')
         
+        # 모델 돌리기
         emotion = _output(url, model_pth)
 
+        # json으로 반환
         return JsonResponse(emotion, status=200)
 
