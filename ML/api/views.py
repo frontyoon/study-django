ed2f340f825faab902dc84124e9e1697726b96c5
frontyoon <issou06261@gmail.com>
2022-06-28 08:04:36 +0900

    delete TestView

--- ML/api/views.py
+++ ML/api/views.py
@@ -12,15 +12,10 @@ class PredictView(View):
         json_object = json.loads(request.body)
         url = json_object['imageUrl']
 
-        img_pth = 'https://consolationbucket.s3.ap-northeast-2.amazonaws.com/b7db6140-7978-465b-be05-f1f2fe058db4.png'
-        model_pth = '/Users/saaeyun/projects/study-django/ML/api/ai/best_model.pth'
+        model_pth = 'ai/best_model.pth'
 
         
         emotion = _output(url, model_pth)
 
         return JsonResponse(emotion, status=200)
 
-class testView(View):
-    def get(self, request):
-        
-        return JsonResponse({"Hello":"World"}, status=200)
